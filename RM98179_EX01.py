3a2fd7e173e519d6763b2a6ec44e0c4c6c909c18
Yuri Cardozo <103226844+Netunosz@users.noreply.github.com>
2023-11-14 02:36:08 -0300

    Desenvolvimento de Algoritmo para Cálculo de Bônus de Acordo com Assinatura e Faturamento Anual.py

--- RM98179_EX01.py
+++ RM98179_EX01.py
@@ -1,29 +1,17 @@
-opcao = int(input("Digite: "
-    "<b> Basic "
-    "<2> Silver "
-    "<3> Gold "
-    "<4> Platinum"))
+assinatura = input("Digite o tipo da sua assinatura: ")
+faturamento = float(input("Digite o valor do seu faturamento: "))
 
-
-while opcao > 0 and opcao < 5:
-    faturamento = float(input("Coloque o valor de seu faturamento anual: "))
-    if opcao == 1:
-        valorBonus = faturamento * (30 / 100)
-        nivel = "Basic"
-        print("Nivel: ".format(nivel) + "\n" + "Faturamento Anual: ".format(faturamento) + "\n" +
-              "Valor Bonus: ".format(valorBonus))
-    elif opcao == 2:
-        valorBonus = faturamento * (20 / 100)
-        nivel = "Silver"
-        print("Nivel: {}".format(nivel) + "\n" + "Faturamento Anual: ".format(faturamento) + "\n" +
-              "Valor Bonus: {}".format(valorBonus))
-    elif opcao == 3:
-        valorBonus = faturamento * (10 / 100)
-        nivel = "Gold"
-        print("Nivel: ".format(nivel) + "\n" + "Faturamento Anual:".format(faturamento) + "\n" +
-              "Valor Bonus: ".format(valorBonus))
-    elif opcao == 4:
-        valorBonus = faturamento * (5 / 100)
-        nivel = "Platinum"
-        print("Nivel: {}".format(nivel) + "\n" + "Faturamento Anual: ".format(faturamento) + "\n" +
-              "Valor Bonus: {}".format(valorBonus))
+if assinatura.upper() == "BASIC":
+    bonus = faturamento * 0.3
+    print("O valor do bônus a ser pago é de R${}".format(bonus))
+elif assinatura.upper() == "SILVER":
+        bonus = faturamento * 0.2
+        print("O valor do bônus a ser pago é de R${}".format(bonus))
+elif assinatura.upper() == "GOLD":
+        bonus = faturamento * 0.1
+        print("O valor do bônus a ser pago é de R${}".format(bonus))
+elif assinatura.upper() == "PLATINUM":
+        bonus = faturamento * 0.05
+        print("O valor do bônus a ser pago é de R${}".format(bonus))
+else:
+    print("Assinatura inválida")
